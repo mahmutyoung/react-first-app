c606529eb7f9fe5fdbc3baeb6d5335cd7c819084
mahmutyoung <54088505+mahmutyoung@users.noreply.github.com>
2022-06-21 17:51:20 +0300

    last changes before sending data upword

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import ExpenseHeader from './components/Expenses/ExpenseHeader';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   return (
       <div>
       <ExpenseHeader/>
+      <NewExpense/>
       <Expenses/>
     </div>
   );
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import './ExpenseForm.css';
+
+
+const ExpenseForm= () => {
+
+  const [enteredTitle,setEnteredTitle]=useState('') 
+  const [enteredAmount,setEnteredAmount]=useState('')
+  const [enteredDate,setEnteredDate]=useState('')
+ 
+  const titleChangeHandler=(event)=>setEnteredTitle(event.target.value);
+  const amountChangeHandler=(event)=>setEnteredAmount(event.target.value);
+  const dateChangeHandler=(event)=>setEnteredDate(event.target.value);
+
+  const submitHandler = (event)=>{
+    event.preventDefault();
+    const data={title: enteredTitle,
+    amount:enteredAmount,
+    date:new Date(enteredDate)}
+    //console.log(data);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  }
+
+  
+  
+  
+/*   const [userInput, setUserInput]=useState({enteredTitle:'',
+                                            enteredAmount:'',
+                                            enteredDate:''})
+                                        
+  const titleChangeHandler=(event)=>{
+    //it does not merge the previous data with the partially updated new data
+    //we have to pass unchanged fields bc it is not a class, it is a function.
+    setUserInput({enteredTitle: event.target.value,
+    enteredAmount:userInput.enteredAmount,
+    enteredDate:userInput.enteredDate})
+
+    //spreading short-cut
+    //setUserInput({...userInput,     
+    //  enteredTitle: event.target.value})
+  }
+
+  const amountChangeHandler=(event)=>{
+    setUserInput({enteredTitle:userInput.enteredTitle,
+    enteredAmount: event.target.value,
+    enteredDate:userInput.enteredDate})
+  }
+  const dateChangeHandler=(event)=>{
+    setUserInput({enteredTitle:userInput.enteredTitle,
+    enteredAmount: userInput.enteredAmount,
+    enteredDate:event.target.value})
+  }
+
+  console.log(userInput) */
+
+  return(
+    <form onSubmit={submitHandler}>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label >Title</label>
+          <input type='text' value={enteredTitle}  onChange={titleChangeHandler} ></input>
+        </div>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input type='number' value={enteredAmount} min='0.1' step='0.1'  onChange={amountChangeHandler} ></input>
+        </div>
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input type='date' value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}></input>
+        </div>
+        <div className='new-expense__actions'>
+          <button type='submit' >Add</button>
+        </div>
+      </div>
+    </form>
+        
+    );
+
+}
+export default ExpenseForm;
